74c776f41bb12b93eea4b29e46698da679e10aaa
dielawn <dielawnmerkill@gmail.com>
2023-01-20 08:43:31 -0700

    form project defined js variables and functions

---
+++ form-validation/script.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form')
+const fullNameInput = document.getElementById('full_name')
+const emailInput = document.getElementById('email')
+const phoneInput = document.getElementById('phone')
+const commentInput = document.getElementById('comment')
+const passwordInput = document.getElementById('password')
+const confirmPasswordInput = document.getElementById('confirmPassword')
+
+
+function validation (){
+
+}
+function errorMsg () {
+
+}
